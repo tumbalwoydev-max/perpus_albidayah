ad97e55f0575d44e8b255f12941245e98ac1134d
irsanwoy <irsanwoy54@gmail.com>
2026-04-01 04:01:14 +0700

    Fix: final pg dialectModule for Vercel

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
-require('pg');
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectModule: require('pg'),
   dialectOptions: {
     ssl: {
       require: true,
